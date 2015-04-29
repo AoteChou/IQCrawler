13dae848910d58a85da6541e00a07c795ae3567f
Aote Zhou <aotechou@gmail.com>
2015-04-29 13:29:42 +0100

    spider set-up and callback

---
+++ src/main/java/com/aote/lodspider/App.java
@@ -0,0 +1,13 @@
+package com.aote.lodspider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
